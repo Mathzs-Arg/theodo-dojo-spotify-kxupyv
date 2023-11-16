f8f703ba1abbc4ce24926e93226dede6d2a2f460
Mathzs-Arg <mathis.metz@gmail.com>
2023-11-16 18:52:12 +0100

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import 'fetchTracks.ts';
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
   'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
@@ -16,7 +17,7 @@ const App = () => {
 		queryKey: ['tracks'],
 		queryFn: fetchTracks
 });
-console.log
+//console.log
   const [trackIndex, setTrackIndex] = useState(0)
   const goToNextTrack = () => {
     setTrackIndex(trackIndex + 1);
